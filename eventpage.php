6d6ed73218e7be273af59661a910a5a3d00ca0ab
trig111 <y364534838@gmail.com>
2014-08-28 17:26:45 -0400

    upload image

--- eventpage.php
+++ eventpage.php
@@ -75,10 +75,14 @@ tinymce.init({
     plugins: [
         "advlist autolink lists link image charmap print preview anchor",
         "searchreplace visualblocks code fullscreen",
-        "insertdatetime media table contextmenu paste"
+        "insertdatetime media table contextmenu paste",
+        "insertdatetime media table contextmenu paste jbimages"
+
     ],
-    toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
+    toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image jbimages",
+    relative_urls: false
 });
+
 </script> ';
 
 
